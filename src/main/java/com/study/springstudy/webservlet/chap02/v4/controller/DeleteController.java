993500dad3a94a9ebf745f755acfab5340d393f4
hyerin11 <hodolin24@naver.com>
2024-05-16 10:03:56 +0900

    #Spring_3_회원가입 정보 저장하기_리펙토링_FrontControllerV4_delete기능_detail기능 추가

---
+++ src/main/java/com/study/springstudy/webservlet/chap02/v4/controller/DeleteController.java
@@ -0,0 +1,23 @@
+package com.study.springstudy.webservlet.chap02.v4.controller;
+
+import com.study.springstudy.webservlet.Model;
+
+import java.util.Map;
+
+import static com.study.springstudy.webservlet.MemberMemoryRepo.repo;
+
+public class DeleteController implements ControllerV4{
+
+    @Override
+    public String process(Map<String, String> paramMap, Model model) {
+
+        // 1. 브라우저에서 삭제 요청이 오면 삭제할 대상의 account를 읽는다.
+       String account = paramMap.get("account");
+
+        // 2. 저장소에 연결하여 삭제처리를 진행시킨다
+        repo.delete(account);
+
+        //3. 삭제가 완료된 화면을 띄우기 위해 조회요청으로 리다이렉션한다.
+        return "redirect:/chap02/v4/show";
+    }
+}
