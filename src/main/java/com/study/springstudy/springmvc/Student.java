c81a367d131c017279a953ab642257c691cf6a29
hyerin11 <hodolin24@naver.com>
2024-05-17 11:48:02 +0900

    #SpringMVC - Lombok student

--- src/main/java/com/study/springstudy/springmvc/Student.java
+++ src/main/java/com/study/springstudy/springmvc/Student.java
@@ -5,10 +5,9 @@ import lombok.*;
 @Setter @Getter
 @ToString
 @EqualsAndHashCode
-@NoArgsConstructor //기본 생성자
-//@AllArgsConstructor //모든 필드 초기화 생성자
-//@RequiredArgsConstructor //필수 파라미터(final 필드) 초기화 생성자
-
+@NoArgsConstructor  // 기본생성자
+@AllArgsConstructor // 모든 필드 초기화 생성자
+//@RequiredArgsConstructor // 필수 파라미터(final 필드) 초기화 생성자
 public class Student {
 
     private String name;
---
+++ src/test/java/com/study/springstudy/springmvc/StudentTest.java
@@ -0,0 +1,18 @@
+package com.study.springstudy.springmvc;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.Struct;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+    @Test
+    void sTest() {
+//        Struct s = new Student();
+//        s.setName("홍길동");
+//        s.setAge(20);
+    }
+
+}
