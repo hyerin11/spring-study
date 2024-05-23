bc503573ef1ccc6942df9ef3ced8ec6d7fc2c946
hyerin11 <hodolin24@naver.com>
2024-05-23 12:27:32 +0900

    #게시판_댓글_비동기통신_1

---
+++ src/main/java/com/study/springstudy/springmvc/chap05/entity/Reply.java
@@ -0,0 +1,39 @@
+package com.study.springstudy.springmvc.chap05.entity;
+
+/*
+*
+* CREATE TABLE tbl_reply(
+	reply_no INT(8) PRIMARY KEY auto_increment,
+    reply_text VARCHAR(1000) NOT NULL,
+    reply_wirter VARCHAR(100) NOT NULL,
+    reply_date DATETIME default current_timestamp,
+    board_no INT(8),
+    constraint fk_reply
+    foreign key (board_no)
+    references tbl_board(board_no)
+    on delete cascade
+);
+*
+* */
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter @ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Reply {
+//1:1 매칭으로 5개 만들어야 함
+
+    private long replyNo;
+
+    @Setter
+    private String replyText;
+    private String replyWriter;
+    private LocalDateTime replyDate;
+    private long boardNo;
+
+}
